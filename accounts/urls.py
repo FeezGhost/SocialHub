4cc4c84af5988f53edec1e37585dd7d63adf5a57
feezghost <feezoocrazy420@gmail.com>
2022-05-01 02:52:11 +0500

    login,  signup templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('login/', views.userLogin, name="login"),
+    path('singup/', views.userSignup, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def userLogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password =request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            greet = "Welcome to the CL Dashboard "+request.user.username
+            messages.info(request, greet)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Username or Password is incorrect! (Contact admin if you think you are blocked)')
+    context = {}
+    return render(request,"accounts/login.html",context)
+
+def userSignup(request):
+    context = {}
+    return render(request,"accounts/signup.html",context)
