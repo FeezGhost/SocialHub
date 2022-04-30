7c8c5f6a0ee7b876062ac04fc487d544ba4b6e20
feezghost <feezoocrazy420@gmail.com>
2022-05-01 03:43:07 +0500

    login, signup ui work

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('login/', views.userLogin, name="login"),
-    path('singup/', views.userSignup, name="signup"),
+    path('signup/', views.userSignup, name="signup"),
     path('logout/', views.logoutView, name="logout"),
     path('home/', views.home, name="home"),
 ]
