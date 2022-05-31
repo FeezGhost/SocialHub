08df87131a5a7f5e286ce67cfafc18efdfd6b19d
feezghost <feezoocrazy420@gmail.com>
2022-06-01 02:13:31 +0500

    content on calender by month

--- calenders/views.py
+++ calenders/views.py
@@ -1,9 +1,30 @@
+import datetime
 from django.shortcuts import render
+from django.contrib import messages
+from utilaties.views import getUserMonthPosts
 
 # Create your views here.
 
 def general(request):
+    currentDate = datetime.date.today()
     client = request.user.client
-    posts = client.client_posts.all()
-    context = {'posts': posts}
+    posts = getUserMonthPosts(request, currentDate)
+    
+    if request.method == "POST":
+        date = request.POST.get('monthHiiden')
+        if date != None:
+            tempDate = datetime.datetime.strptime(date, "%Y-%m-%d")
+            newdate = tempDate.strftime("%Y-%m-%d")
+            posts = getUserMonthPosts(request, tempDate)
+            context = {
+                'posts' : posts, 
+                "date" : newdate,
+                }
+            return render(request,"calenders/calenderListing.html",context)
+        else:
+            messages.error(request, "There was a date issue!")
+    context = {
+        'posts': posts,  
+        "date" : currentDate,
+        }
     return render(request,"calenders/calenderListing.html",context)
--- utilaties/views.py
+++ utilaties/views.py
@@ -1,3 +1,18 @@
+import datetime
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def getMonths(date):
+    currentMonth = int(date.strftime("%m"))
+    currentYear = int(date.strftime("%Y"))
+    thisMonthDate = datetime.date(year=currentYear,month=currentMonth,day=1)
+    nextMonthDate = datetime.date(year=currentYear,month=(currentMonth+1),day=1)
+    return thisMonthDate, nextMonthDate
+    
+def getUserMonthPosts(request, date):
+    user = request.user.client
+    thisMonthDate, nextMonthDate = getMonths(date)
+    products = user.client_posts.all().filter(dateToBuy__gte=thisMonthDate,dateToBuy__lt=nextMonthDate)
+    return products
